adf9f108ae1cda1f73eb1b7653390fb6b024a809
NerwenElensar <jenny2209@gmx.de>
2015-10-08 20:05:50 +0200

    Klassen für Tests angelegt, Test für Scenegraph auch noch?

---
+++ Computergraphics/src/tests/rotationTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class rotationTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
