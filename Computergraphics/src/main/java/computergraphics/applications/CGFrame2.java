86f32c062000e331d8ef1e7222738871442678e6
Robert <rscheffel00@gmail.com>
2015-10-17 12:04:28 +0200

    Heli fliegt hin und her

--- Computergraphics/src/main/java/computergraphics/applications/CGFrame2.java
+++ Computergraphics/src/main/java/computergraphics/applications/CGFrame2.java
@@ -33,18 +33,21 @@ public class CGFrame2 extends AbstractCGFrame {
 	 */
 	private static final long serialVersionUID = 4257130065274995543L;
 	
-	private static final int ROTORHEADANGLE = 40;
+	private static final int ROTORHEAD_ANGLE = 40;
+	private static final double EPSILON = 0.2;
 	
+	private HelicopterNode heli;
 	private TranslationNode translationNodeHelicopter;
+	private Vector3 positionHeli;
 	private RotationNode rotateNodeHeli;
-	HelicopterNode heli;
-	private double xPos = 1;
-	private double zPos = 1;
-	private double rotorheadAngle = 1;
-	private double heliAngle = 10;
+	private Vector3 rotationHeli;
+	private double heliAngle = 0;
 	
-	private double xSizeGround = 10;
-	private double zSizeGround = 15;
+	private double rotorheadAngle = 1;
+	
+	private final double X_SIZE_GROUND = 15;
+	private final double Z_SIZE_GROUND = 20;
+	private int direction = 1;
 	
 	/**
 	 * Constructor.
@@ -52,6 +55,8 @@ public class CGFrame2 extends AbstractCGFrame {
 	public CGFrame2(int timerInverval) {
 		super(timerInverval);
 		
+		positionHeli = new Vector3(0.0, 0.9, 0.0);
+		rotationHeli = new Vector3(0.0, 1.0, 0.0);
 		// Shader node does the lighting computation
 		ShaderNode shaderNode = new ShaderNode(ShaderType.PHONG);
 		getRoot().addChild(shaderNode);
@@ -59,53 +64,66 @@ public class CGFrame2 extends AbstractCGFrame {
 		// Ground
 		TranslationNode translationNodeBoden = new TranslationNode(new Vector3(0.0, -0.25, 0.0));
 		shaderNode.addChild(translationNodeBoden);
-		CuboidNode boden = new CuboidNode(xSizeGround, 0.001, zSizeGround,new Vector3(0.8, 0.4, 0.1));
+		CuboidNode boden = new CuboidNode(X_SIZE_GROUND, 0.001, Z_SIZE_GROUND, new Vector3(0.8, 0.4, 0.1));
 		ColorNode colorNodeGround = new ColorNode(0.0, 0.5, 0.2);
 		translationNodeBoden.addChild(colorNodeGround);
 		colorNodeGround.addChild(boden);
 		
-		
-		
-		
 		// Duplicate Node
 		DuplicateNode duplicateTreesNode = new DuplicateNode(5);
 		shaderNode.addChild(duplicateTreesNode);
+		
 		// Single Tree
 		TranslationNode translationNodeTrees = new TranslationNode(new Vector3(0.2, 0.0, 0.0));
 		duplicateTreesNode.addChild(translationNodeTrees);
 		BaumNode baum1 = new BaumNode();
 		translationNodeTrees.addChild(baum1);
 		
-		
-		//Forrest
+		// Forrest
 		TranslationNode translationNodeForrest = new TranslationNode(new Vector3(0.0, 0.0, 0.0));
 		shaderNode.addChild(translationNodeForrest);
 		translationNodeForrest.addChild(new ForrestNode());
 		
-		//Helicopter
-		translationNodeHelicopter = new TranslationNode(new Vector3(2.85, 0.85, 0.0));
+		// Helicopter
+		translationNodeHelicopter = new TranslationNode(positionHeli);
 		shaderNode.addChild(translationNodeHelicopter);
-		rotateNodeHeli = new RotationNode(new Vector3(0.0, 1.0, 0.0), 0);
+		rotateNodeHeli = new RotationNode(rotationHeli, heliAngle);
 		translationNodeHelicopter.addChild(rotateNodeHeli);
+		
 		heli = new HelicopterNode();
 		rotateNodeHeli.addChild(heli);
-		
 		
 	}
 	
 	@Override
 	protected void timerTick() {
 		System.out.println("Tick");
-		xPos = xPos - 0.1;
-		zPos = zPos - 0.1;
-		rotorheadAngle = rotorheadAngle + ROTORHEADANGLE;
+		
+		rotorheadAngle = rotorheadAngle + ROTORHEAD_ANGLE;
 		this.heli.getRotorhead().setAngle(rotorheadAngle);
 		
-		heliAngle = heliAngle + 10;
-		this.translationNodeHelicopter.setPosition(new Vector3(xPos, 1, 0));
+		double xPos = positionHeli.get(0) - 0.1 * direction;
+		double yPos = positionHeli.get(1);
+		double zPos = positionHeli.get(2);
+		System.out.println(direction);
+		if(xPos < X_SIZE_GROUND / 3 && (xPos > X_SIZE_GROUND / 3 * -1) && (Math.abs(heliAngle - 0.0) < EPSILON)) {
+			positionHeli.copy(new Vector3(xPos, yPos, zPos));
+			this.translationNodeHelicopter.setPosition(positionHeli);
+		}
+		else if(xPos < X_SIZE_GROUND / 3 && (xPos > X_SIZE_GROUND / 3 * -1) && (Math.abs(heliAngle - 180.0) < EPSILON)) {
+			positionHeli.copy(new Vector3(xPos, yPos, zPos));
+			this.translationNodeHelicopter.setPosition(positionHeli);
+		}
+		else if(heliAngle < 180.0 && xPos < 0.0){
+			heliAngle = heliAngle + 10;
+			this.rotateNodeHeli.setAngle(heliAngle);
+				direction = -1;
+		}else if(heliAngle > 0.0){
+			heliAngle = heliAngle - 10;
+			this.rotateNodeHeli.setAngle(heliAngle);
+				direction = 1;
+		}
 
-//		this.rotateNodeHeli.setAngle(heliAngle);;
-		
 		
 	}
 	
--- Computergraphics/src/main/java/computergraphics/scenegraph/HelicopterNode.java
+++ Computergraphics/src/main/java/computergraphics/scenegraph/HelicopterNode.java
@@ -14,51 +14,45 @@ public class HelicopterNode extends GroupNode {
 	private RotationNode rotateRotorhead;
 	
 	public HelicopterNode(){
-		//Hull
-		SphereNode hull = new SphereNode(0.20, 15,0.0,0.7,0.5);
-		addChild(hull);
-		
-		
-		TranslationNode translationNode = new TranslationNode(new Vector3(0.35, 0.065, 0.0));
-		addChild(translationNode);
-		translationNode.addChild(new CuboidNode(0.45, 0.16, 0.18));
-		
-		
-		TranslationNode translationNodeHullTop = new TranslationNode(new Vector3(0.15, 0.15, 0.0));
-		addChild(translationNodeHullTop);
-		
-		translationNodeHullTop.addChild(new CuboidNode(0.15, 0.15, 0.15));
-		
-		//Skids Kuven
-		TranslationNode translationNodeSkidsOne = new TranslationNode(new Vector3(0.0, -0.17, 0.09));
-		addChild(translationNodeSkidsOne);
-		
-		translationNodeSkidsOne.addChild(new CuboidNode(0.5, 0.09, 0.09));
-		
-		//Skids
-		TranslationNode translationNodeSkidsTwo = new TranslationNode(new Vector3(0.0, -0.17, -0.09));
-		addChild(translationNodeSkidsTwo);
-		
-		translationNodeSkidsTwo.addChild(new CuboidNode(0.5, 0.09, 0.09));
-		
-		//Window
-		TranslationNode translationNodeWindow = new TranslationNode(new Vector3(-0.07, 0.03, 0.0));
-		addChild(translationNodeWindow);
-		translationNodeWindow.addChild(new SphereNode(0.15, 15, new Vector3(0.1, 0.2, 0.7)));
-		
-		//Rotorhead
-		TranslationNode translationNodeRotorHead = new TranslationNode(new Vector3(0.15, 0.25, 0.0));
-		addChild(translationNodeRotorHead);
-		
-		rotateRotorhead = new RotationNode(new Vector3(0.0, 1.0, 0.0), 0);
-		translationNodeRotorHead.addChild(rotateRotorhead);
-	
-		rotateRotorhead.addChild(new RotorHeadNode());
+		this(0.0,0.0,0.0);
 	}
 	
 	
 	public HelicopterNode(double xPosition, double yPosition, double zPosition){
 		
+		//Hull
+		SphereNode hull = new SphereNode(0.20, 15,0.0,0.7,0.5);
+		addChild(hull);
+		
+		TranslationNode translationNode = new TranslationNode(new Vector3(0.35, 0.065, 0.0));
+		translationNode.addChild(new CuboidNode(0.45, 0.16, 0.18));
+		addChild(translationNode);
+		
+		TranslationNode translationNodeHullTop = new TranslationNode(new Vector3(0.15, 0.15, 0.0));
+		translationNodeHullTop.addChild(new CuboidNode(0.15, 0.15, 0.15));
+		addChild(translationNodeHullTop);
+		
+		//Skids Kuven
+		TranslationNode translationNodeSkidsOne = new TranslationNode(new Vector3(0.0, -0.17, 0.09));
+		translationNodeSkidsOne.addChild(new CuboidNode(0.5, 0.09, 0.09));
+		addChild(translationNodeSkidsOne);
+		
+		//Skids
+		TranslationNode translationNodeSkidsTwo = new TranslationNode(new Vector3(0.0, -0.17, -0.09));
+		translationNodeSkidsTwo.addChild(new CuboidNode(0.5, 0.09, 0.09));
+		addChild(translationNodeSkidsTwo);
+		
+		//Window
+		TranslationNode translationNodeWindow = new TranslationNode(new Vector3(-0.07, 0.03, 0.0));
+		translationNodeWindow.addChild(new SphereNode(0.15, 15, new Vector3(0.1, 0.2, 0.7)));
+		addChild(translationNodeWindow);
+		
+		//Rotorhead
+		TranslationNode translationNodeRotorHead = new TranslationNode(new Vector3(0.15, 0.25, 0.0));
+		rotateRotorhead = new RotationNode(new Vector3(0.0, 1.0, 0.0), 0);
+		translationNodeRotorHead.addChild(rotateRotorhead);
+		rotateRotorhead.addChild(new RotorHeadNode());
+		addChild(translationNodeRotorHead);
 	}
 	
 	public RotationNode getRotorhead(){
