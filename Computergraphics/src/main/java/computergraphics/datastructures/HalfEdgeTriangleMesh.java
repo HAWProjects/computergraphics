07497378b77526fba8626803f50c4458ad008ba7
Robert <rscheffel00@gmail.com>
2015-11-03 19:25:54 +0100

    leider kein ergebnis - computetrianglenormal angefangen und zweiter ansatz für addtriangle

--- Computergraphics/src/main/java/computergraphics/datastructures/HalfEdgeTriangleMesh.java
+++ Computergraphics/src/main/java/computergraphics/datastructures/HalfEdgeTriangleMesh.java
@@ -6,20 +6,24 @@
 */
 package main.java.computergraphics.datastructures;
 
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+
+import main.java.computergraphics.math.Vector3;
 
 /**
  * @author Robert Scheffel, Jennifer Momsen
- *
+ * 		
  */
 public class HalfEdgeTriangleMesh implements ITriangleMesh {
-
+	
 	private List<Vertex> vList;
 	private List<HalfEdge> hEList;
 	private List<TriangleFacet> tFList;
-
+	
 	/**
 	 * Constructor
 	 */
@@ -28,17 +32,14 @@ public class HalfEdgeTriangleMesh implements ITriangleMesh {
 		this.hEList = new ArrayList<>();
 		this.tFList = new ArrayList<>();
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * main.java.computergraphics.datastructures.ITriangleMesh#addTriangle(int,
-	 * int, int)
+	 * @see main.java.computergraphics.datastructures.ITriangleMesh#addTriangle(int, int, int)
 	 */
 	@Override
 	public void addTriangle(int vertexIndex1, int vertexIndex2, int vertexIndex3) {
-		
 		
 		List<Vertex> tempVList = new ArrayList<>();
 		tempVList.add(vList.get(vertexIndex1));
@@ -46,19 +47,19 @@ public class HalfEdgeTriangleMesh implements ITriangleMesh {
 		tempVList.add(vList.get(vertexIndex3));
 		
 		List<HalfEdge> tempHeList = new ArrayList<>();
-		for(int i = 0 ; i< 3;i++){
+		for(int i = 0; i < 3; i++) {
 			tempHeList.add(new HalfEdge());
 		}
 		
-		if (!tFList.isEmpty() ) {
+		if(!tFList.isEmpty()) {
 			
-			for (TriangleFacet facet : tFList) {
+			for(TriangleFacet facet: tFList) {
 				
 				HalfEdge halfEdge = facet.getHalfEdge();
 				Vertex v1 = halfEdge.getStartVertex();
 				Vertex v2 = halfEdge.getNext().getStartVertex();
 				
-				if(tempVList.contains(v1)&& tempVList.contains(v2)){
+				if(tempVList.contains(v1) && tempVList.contains(v2)) {
 					TriangleFacet tF = new TriangleFacet();
 					
 					HalfEdge tempHalfEdge1 = new HalfEdge();
@@ -75,37 +76,95 @@ public class HalfEdgeTriangleMesh implements ITriangleMesh {
 				}
 			}
 			
-		} else {
-			for (int i = 0; i < 3; i++) {
+		}
+		else {
+			for(int i = 0; i < 3; i++) {
 				tempHeList.get(i).setStartVertex(tempVList.get(i));
-				tempHeList.get(i).setNext(tempHeList.get((i+1) % 3));
-				hEList.add(tempHeList.get(i)); 
+				tempHeList.get(i).setNext(tempHeList.get((i + 1) % 3));
+				hEList.add(tempHeList.get(i));
 			}
 			
 		}
-
+		
 		calculateOppositeHalfEdge();
 	}
-
-
-
+	
+	private void alternativerVersuch(int vertexIndex1, int vertexIndex2, int vertexIndex3) {
+		List<Vertex> tempVList = new ArrayList<>();
+		tempVList.add(vList.get(vertexIndex1));
+		tempVList.add(vList.get(vertexIndex2));
+		tempVList.add(vList.get(vertexIndex3));
+		
+		List<HalfEdge> tempHeList = new ArrayList<>();
+		for(int i = 0; i < 3; i++) {
+			tempHeList.add(new HalfEdge());
+		}
+		
+		if(!tFList.isEmpty()) {
+			boolean isNotSameFace = true;
+			for(TriangleFacet facet: tFList) {
+				
+				HalfEdge halfEdge1 = facet.getHalfEdge();
+				HalfEdge halfEdge2 = halfEdge1.getNext();
+				HalfEdge halfEdge3 = halfEdge1.getNext().getNext();
+				List<HalfEdge> secondHalfEdgeList = new ArrayList<>();
+				secondHalfEdgeList.add(halfEdge1);
+				secondHalfEdgeList.add(halfEdge2);
+				secondHalfEdgeList.add(halfEdge3);
+				
+				for(int k = 0; k < 3; k++) {
+					if(tempVList.contains(secondHalfEdgeList.get(k).getStartVertex())
+							&& tempVList.contains(secondHalfEdgeList.get((k + 1) % 3).getStartVertex())) {
+						TriangleFacet tF = new TriangleFacet();
+						
+						HalfEdge tempHalfEdge1 = new HalfEdge();
+						HalfEdge tempHalfEdge2 = new HalfEdge();
+						// alles setzen
+						
+						isNotSameFace = false; // angrenzendes Vertex gefunden
+						tFList.add(tF);
+						break;
+					}
+				}
+			}
+			if(isNotSameFace) {
+				for(int i = 0; i < 3; i++) {
+					tempHeList.get(i).setStartVertex(tempVList.get(i));
+					tempHeList.get(i).setNext(tempHeList.get((i + 1) % 3));
+					hEList.add(tempHeList.get(i));
+				}
+				isNotSameFace = true;
+			}
+			
+		}
+		else {
+			for(int i = 0; i < 3; i++) {
+				tempHeList.get(i).setStartVertex(tempVList.get(i));
+				tempHeList.get(i).setNext(tempHeList.get((i + 1) % 3));
+				hEList.add(tempHeList.get(i));
+			}
+			
+		}
+		
+		calculateOppositeHalfEdge();
+	}
+	
 	/**
 	 * 
 	 */
 	private void calculateOppositeHalfEdge() {
-		for (Iterator<HalfEdge> itHalfEdge = hEList.iterator(); itHalfEdge.hasNext();) {
+		for(Iterator<HalfEdge> itHalfEdge = hEList.iterator(); itHalfEdge.hasNext();) {
 			HalfEdge hE = itHalfEdge.next();
 			Vertex vTarget = hE.getNext().getStartVertex();
 			hE.setOpposite(vTarget.getHalfEdge());
 		}
-
+		
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * main.java.computergraphics.datastructures.ITriangleMesh#addVertex(main.
+	 * @see main.java.computergraphics.datastructures.ITriangleMesh#addVertex(main.
 	 * java.computergraphics.datastructures.Vertex)
 	 */
 	@Override
@@ -113,51 +172,47 @@ public class HalfEdgeTriangleMesh implements ITriangleMesh {
 		vList.add(v);
 		return vList.indexOf(v);
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see main.java.computergraphics.datastructures.ITriangleMesh#
-	 * getNumberOfTriangles()
+	 * @see main.java.computergraphics.datastructures.ITriangleMesh# getNumberOfTriangles()
 	 */
 	@Override
 	public int getNumberOfTriangles() {
 		return tFList.size();
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see main.java.computergraphics.datastructures.ITriangleMesh#
-	 * getNumberOfVertices()
+	 * @see main.java.computergraphics.datastructures.ITriangleMesh# getNumberOfVertices()
 	 */
 	@Override
 	public int getNumberOfVertices() {
 		return vList.size();
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * main.java.computergraphics.datastructures.ITriangleMesh#getVertex(int)
+	 * @see main.java.computergraphics.datastructures.ITriangleMesh#getVertex(int)
 	 */
 	@Override
 	public Vertex getVertex(int index) {
 		return vList.get(index);
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * main.java.computergraphics.datastructures.ITriangleMesh#getFacet(int)
+	 * @see main.java.computergraphics.datastructures.ITriangleMesh#getFacet(int)
 	 */
 	@Override
 	public TriangleFacet getFacet(int facetIndex) {
 		return tFList.get(facetIndex);
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -168,41 +223,52 @@ public class HalfEdgeTriangleMesh implements ITriangleMesh {
 		tFList.clear();
 		vList.clear();
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see main.java.computergraphics.datastructures.ITriangleMesh#
-	 * computeTriangleNormals()
+	 * @see main.java.computergraphics.datastructures.ITriangleMesh# computeTriangleNormals()
 	 */
 	@Override
 	public void computeTriangleNormals() {
-		// TODO Auto-generated method stub
-
+		for(ListIterator<TriangleFacet> itTF = tFList.listIterator(); itTF.hasNext();){
+			TriangleFacet tempFacet = itTF.next();
+			//berechne Normale 
+			Vector3 v1,v2,v3;
+			v1 = tempFacet.getHalfEdge().getStartVertex().getPosition();
+			v2 = tempFacet.getHalfEdge().getNext().getStartVertex().getPosition();
+			v3 = tempFacet.getHalfEdge().getNext().getNext().getStartVertex().getPosition();
+			//spannvectoren
+			
+			//kreuzprodukt
+			
+			//normieren
+			
+			//setze normale
+		}
+		
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see main.java.computergraphics.datastructures.ITriangleMesh#
-	 * setTextureFilename(java.lang.String)
+	 * @see main.java.computergraphics.datastructures.ITriangleMesh# setTextureFilename(java.lang.String)
 	 */
 	@Override
 	public void setTextureFilename(String filename) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see main.java.computergraphics.datastructures.ITriangleMesh#
-	 * getTextureFilename()
+	 * @see main.java.computergraphics.datastructures.ITriangleMesh# getTextureFilename()
 	 */
 	@Override
 	public String getTextureFilename() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
 }
