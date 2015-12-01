176804d323b1beb96db68cad6e3e33426626508b
Robert <rscheffel00@gmail.com>
2015-12-01 20:37:44 +0100

    Hab es probiert aber leider keinen test geschafft

---
+++ Computergraphics/src/tests/MarchingCubesAlgorithmTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.computergraphics.datastructures.MarchingCubesAlgorithm;
+
+
+public class MarchingCubesAlgorithmTest {
+	
+	private MarchingCubesAlgorithm mca;
+	
+	@Before
+	public void setUp() throws Exception {
+		mca = new MarchingCubesAlgorithm();
+	}
+	
+	@Test
+	public void testcreateTriangle() {
+
+	}
+	
+}
