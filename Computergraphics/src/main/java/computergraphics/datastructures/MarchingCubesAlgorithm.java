3a732ead7f3bba7c2fa539615bdd73444ad84f96
Jenny <jenny2209@gmx.de>
2015-12-01 19:19:38 +0100

    add Triangle soweit fertig, Tests fehlen noch

--- Computergraphics/src/main/java/computergraphics/datastructures/MarchingCubesAlgorithm.java
+++ Computergraphics/src/main/java/computergraphics/datastructures/MarchingCubesAlgorithm.java
@@ -11,17 +11,20 @@ import main.java.computergraphics.math.Vector3;
 public class MarchingCubesAlgorithm {
 
 	private HalfEdgeTriangleMesh mesh;
+	Map<Integer, Vector3> tempMap = new HashMap<>();
+	List<Vertex> vertexList = new ArrayList<>();
+	private double TAU = 0;
 
 	public MarchingCubesAlgorithm() {
 		this.mesh = new HalfEdgeTriangleMesh();
 	}
 
-	private void createTriangle(List<Vector3> points, List<Double> values) {
+	public void createTriangle(List<Vector3> points, List<Double> values) {
 		int caseIndex;
-		double t = 0.5; //Approximation des Eckpunktes für den Anfang
+		double t = 0.5; // Approximation des Eckpunktes fuer den Anfang
 		List<Integer> bList = new ArrayList<>();
 		for (int i = 0; i < 8; i++) {
-			if (values.get(i) > 0) {
+			if (values.get(i) > TAU) {
 				bList.add(1);
 			} else {
 				bList.add(0);
@@ -29,7 +32,7 @@ public class MarchingCubesAlgorithm {
 		}
 		caseIndex = bList.get(0) * 1 + bList.get(1) * 2 + bList.get(2) * 4 + bList.get(3) * 8 + bList.get(4) * 16
 				+ bList.get(5) * 32 + bList.get(6) * 64 + bList.get(7) * 128;
-		//CaseIndex benutzen für LookUp Tabelle: ab Index + 15
+		// CaseIndex benutzen fuer LookUp Tabelle: ab Index + 15
 
 		int endIndex = (caseIndex + 1) * 15 - 1;
 		List<Integer> edgeList = new ArrayList<>();
@@ -38,26 +41,23 @@ public class MarchingCubesAlgorithm {
 				edgeList.add(Constants.FACES[index]);
 			}
 		}
-		Map<Integer, Vector3> tempMap = new HashMap();
-		int triangleCount = edgeList.size()/3;
-		edgeList.forEach((edge) -> {
-			while (!(edgeList.isEmpty())) {
-				for (int i = 0; i < 3; i++) {
-					int[] p = getIndeces(edgeList.get(i));
 
-					Vector3 pos = (points.get(p[0]).multiply(1 - t)).add(points.get(p[1]).multiply(t));
-					tempMap.put(edgeList.get(i), pos);
-				}
-				edgeList.remove(0);
-				edgeList.remove(1);
-				edgeList.remove(2);
+		int indexCount = 0;
+		while (!(edgeList.isEmpty())) {
+			for (int i = 0; i < 3; i++) {
+				int[] p = getIndeces(edgeList.get(i));
+				// Knoten hinzufÃ¼gen
+				Vector3 pos = (points.get(p[0]).multiply(1 - t)).add(points.get(p[1]).multiply(t));
+				Vertex v = new Vertex(pos);
+				mesh.addVertex(v);
 			}
-		});
-		//Knoten geben lassen
-//		mesh.addTriangle(vertexIndex1, vertexIndex2, vertexIndex3);
-
-
-
+			// knoten hinzufÃ¼gen plus dreieck
+			mesh.addTriangle(indexCount, indexCount + 1, indexCount + 2);
+			indexCount += 3;
+			edgeList.remove(0);
+			edgeList.remove(1);
+			edgeList.remove(2);
+		}
 	}
 
 	private int[] getIndeces(int edge) {
